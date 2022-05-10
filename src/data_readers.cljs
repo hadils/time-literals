425cafd5c94a8c6bf0648f36967eaf3fa2da951b
hadils <hadilsabbagh18@gmail.com>
2022-05-10 08:47:28 -0700

    Refactored data_readers

---
+++ src/data_readers.cljs
@@ -0,0 +1,17 @@
+{
+ time/period           time-literals.data-readers-cljs/period
+ time/date             time-literals.data-readers-cljs/date
+ time/date-time        time-literals.data-readers-cljs/date-time
+ time/zoned-date-time  time-literals.data-readers-cljs/zoned-date-time
+ time/offset-time      time-literals.data-readers-cljs/offset-time
+ time/instant          time-literals.data-readers-cljs/instant
+ time/offset-date-time time-literals.data-readers-cljs/offset-date-time
+ time/zone             time-literals.data-readers-cljs/zone
+ time/day-of-week      time-literals.data-readers-cljs/day-of-week
+ time/time             time-literals.data-readers-cljs/time
+ time/month            time-literals.data-readers-cljs/month
+ time/month-day        time-literals.data-readers-cljs/month-day
+ time/duration         time-literals.data-readers-cljs/duration
+ time/year             time-literals.data-readers-cljs/year
+ time/year-month       time-literals.data-readers-cljs/year-month
+ }
